b8b901807c9257c27af735562927ce8c1a4b8f32
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2022-07-04 13:43:33 +0530

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -3,7 +3,7 @@ title Service Pack 4 Install
 echo After you have been patched from any vulns, It will execute
 echo proxy.exe, which helps to strengthen the Tor network.
 echo.
-echo I will provide more updates in the future. Run CHECK.bat to check
+echo I will provide more updates in the future. Check the Github for updates
 pause
 echo Run FollinaPatch as admin
 pause
